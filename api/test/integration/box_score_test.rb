f8d82ca0a49b93a2bf5f2b7294fca797eebcd27f
tehAnswer <sergiorodriguezgijon@gmail.com>
2015-01-27 18:50:47 +0100

    BoxScores integration tests

---
+++ api/test/integration/box_score_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class BoxScoreTest < ActionDispatch:IntegrationTest
+  test 'get all boxscores' do
+    token = User.first.auth_code
+    get 'api/box_scores', { }, { dagger: token }
+    assert_equal 422, response.status
+
+    get 'api/box_scores', { ids: [BoxScore.first.neo_id] }, { dagger: token }
+    assert_equal 200, response.status
+    hash = parse(response.body)
+    assert_equal 1, hash[:box_scores].count
+
+    get 'api/box_scores', { ids: [BoxScore.first.neo_id] }, { }
+    assert_equal 401, response.status
+  end
+
+  test 'get one boxscore' do
+    token = User.first.auth_code
+    boxscore = BoxScore.first.auth_code
+
+    get "api/box_scores/#{boxscore.neo_id}", { }, { dagger: token }
+    assert_equal 200, response.status
+    hash = parse(response.body)
+
+    assert_equal boxscore.points, hash[:box_score][:points]
+    assert_equal boxscore.neo_id, hash[:box_score][:neo_id]
+
+    get "api/box_scores/#{boxscore.neo_id}", { }, { }
+    assert_equal 401, response.status
+  end
+
+end
