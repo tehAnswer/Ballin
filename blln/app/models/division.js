228d50d3d4ce8489778cadbf66476cd8a7c9ba0a
Sergio G <sergiorodriguezgijon@gmail.com>
2015-02-07 16:57:51 +0100

    Some CSS magic

--- blln/app/models/division.js
+++ blln/app/models/division.js
@@ -2,5 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr("string"),
-  teams: DS.hasMany("fantasticTeam")
+  teams: DS.hasMany("fantasticTeam"),
+  conference: DS.belongsTo("conference"),
+  hasFreeSpace: Ember.computed('teams', function() {
+  	return this.get('teams.length') < 5;
+  }),
+  isFull: Ember.computed.not('hasFreeSpace')
 });
