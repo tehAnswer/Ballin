6eafea19e33047edce996dd86167ccf466e49c27
Sergio G <sergiorodriguezgijon@gmail.com>
2015-02-11 21:09:31 +0100

    League unit test

--- api/test/models/league_test.rb
+++ api/test/models/league_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class LeagueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name length" do
+    league = League.new
+    assert_equal false, league.valid?
+    league.name = "a"
+    assert_equal false, league.valid?
+    league.name = "aaa"
+    assert_equal false, league.valid?
+    league.name = "aaaaa"
+    assert_equal true, league.valid?
+    league.name = "El Señor de los Anillos"
+    assert_equal true, league.valid?
+    league.name += ": Las dos Torres"
+    assert_equal false, league.valid?
+  end
 end
