437871626bceafed805daa6ba3890a919373abd5
Sergio G <sergiorodriguezgijon@gmail.com>
2015-03-16 13:22:24 +0100

    Small changes

--- api/app/models/roster_slot.rb
+++ api/app/models/roster_slot.rb
@@ -3,15 +3,11 @@ class RosterSlot
   from_class Rotation
   to_class Player
 
-  validate :has_contract, :has_been_already_selected
+  validate :has_contract
 
   
   def has_contract
     self.errors.add(:has_contract, "#{to_node.name} has not contract with #{from_node.team.name}") unless from_node.team.players.include?(to_node)
-  end
-
-  def has_been_already_selected
-    self.errors.add(:has_been_already_selected, "#{to_node.name} has been already selected") if from_node.players.select { |player| player == to_node }.count > 1
   end
 
   def can_play(main_position, second_position = '')
