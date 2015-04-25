013b8d6ea3e8629b84f0bb2592838df894163e47
Sergio G <sergiorodriguezgijon@gmail.com>
2015-04-25 14:31:34 +0200

    Basic i18n support.

---
+++ blln/app/locales/en.js
@@ -0,0 +1,33 @@
+export default {
+  learnMore: 'Learn More',
+  headline: 'A fantasy league you can trust.',
+  home: 'Home',
+  actions: {
+    loading: 'Loading',
+    close: 'Close',
+    submit: 'Submit',
+    makeBid: 'Make bid',
+    signUp: 'Sign up',
+    register: 'Register',
+    login: 'Login'
+  },
+  myLeague: {
+    league: 'League'
+  },
+  market: {
+    auctions: 'Auctions',
+    modalHeaderTitle: 'Make your bid',
+    placeholder: 'Enter your offer' 
+  },
+  tableHeaders: {
+    position: 'POSITION',
+    player: 'PLAYER',
+    score: 'SCORE',
+    maxBid: 'MAX BID',
+    endsIn: 'ENDS IN'
+  },
+  footer: {
+    beforeLink: 'A',
+    afterLink: 'masterpiece'
+  }
+};
