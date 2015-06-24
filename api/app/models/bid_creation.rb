5907cdc6148f6966817fec7bff8dc60bb6588633
tehAnswer <sergiorodriguezgijon@gmail.com>
2015-06-24 20:36:11 +0200

    Fixed error

--- api/app/models/bid_creation.rb
+++ api/app/models/bid_creation.rb
@@ -7,6 +7,7 @@ class BidCreation
       check(user.team.nil?, "You cant make a bid if you have not a team")
       check(user.team.budget < params[:salary].to_f, "You cant make a bid over your budget")
       check(user.team.league != auction.league, "You cant bid in other leagues auction")
+      return false unless errors.empty?
       self.bid = Bid.create!(params)
       bid_rel = BidRelation.create(from_node: auction, to_node: self.bid)
       bid.team = user.team
